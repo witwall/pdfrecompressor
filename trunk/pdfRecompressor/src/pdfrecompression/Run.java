dcfc973b0e947f699bb73ba412e511d3560adaca
hata.radim <hata.radim@gmail.com>
2010-10-12 11:52:11 +0000

    finished missing javadoc in code and small corrections

--- trunk/pdfRecompressor/src/pdfrecompression/Run.java
+++ trunk/pdfRecompressor/src/pdfrecompression/Run.java
@@ -4,12 +4,9 @@
  */
 package pdfrecompression;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.HashSet;
 import java.util.List;
