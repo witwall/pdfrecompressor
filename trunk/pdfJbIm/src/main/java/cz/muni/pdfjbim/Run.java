2f2a4ff4b6301cff60e8e37d8083fc0b6c69f3b2
hata.radim <hata.radim@3795e106-5119-e3f0-1bc5-75bb0a249856>
2010-12-29 12:27:41 +0000

    pdfJbIm changed to be a maven project

---
+++ trunk/pdfJbIm/src/main/java/cz/muni/pdfjbim/Run.java
@@ -0,0 +1,258 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.pdfjbim;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ *
+ * @author Radim Hatlapatka (208155@mail.muni.cz)
+ * @version 1.0
+ */
+public class Run {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws PdfRecompressionException {
+        if (args.length < 4) {
+            usage();
+        }
+
+        String jbig2enc = null;
+        String pdfFile = null;
+        String outputPdf = null;
+        String password = null;
+        double defaultThresh = 0.85;
+        int bwThresh = 188;
+        Boolean autoThresh = false;
+        Set<Integer> pagesToProcess = null;
+        Boolean silent = false;
+        Boolean binarize = false;
+        String basename = "output";
+
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equalsIgnoreCase("-h")) {
+                usage();
+            }
+            if (args[i].equalsIgnoreCase("-input")) {
+                i++;
+                if (i >= args.length) {
+                    usage();
+                }
+                pdfFile = args[i];
+                continue;
+            }
+
+            if (args[i].equalsIgnoreCase("-pathToEnc")) {
+                i++;
+                if (i >= args.length) {
+                    usage();
+                }
+                jbig2enc = args[i];
+                continue;
+            }
+
+            if (args[i].equalsIgnoreCase("-output")) {
+                i++;
+                if (i >= args.length) {
+                    usage();
+                }
+                outputPdf = args[i];
+                continue;
+            }
+
+            if (args[i].equalsIgnoreCase("-passwd")) {
+                i++;
+                if (i >= args.length) {
+                    usage();
+                }
+                password = args[i];
+                continue;
+            }
+
+            if (args[i].equalsIgnoreCase("-basename")) {
+                i++;
+                if (i >= args.length) {
+                    usage();
+                }
+                basename = args[i];
+                continue;
+            }
+
+            if (args[i].equalsIgnoreCase("-thresh")) {
+                i++;
+                if (i >= args.length) {
+                    usage();
+                }
+
+                defaultThresh = Double.parseDouble(args[i]);
+                if ((defaultThresh > 0.9) || (defaultThresh < 0.5)) {
+                    System.err.println("Invalid threshold value: (0.5..0.9)\n");
+                    usage();
+                }
+                continue;
+            }
+
+            if (args[i].equalsIgnoreCase("-bw_thresh")) {
+                i++;
+                if (i >= args.length) {
+                    usage();
+                }
+
+                bwThresh = Integer.parseInt(args[i]);
+                if ((bwThresh < 0) || (bwThresh > 255)) {
+                    System.err.println("Invalid bw threshold value: (0..255)\n");
+                    usage();
+                }
+                continue;
+            }
+
+            if (args[i].equalsIgnoreCase("-binarize")) {
+                binarize = true;
+                continue;
+            }
+
+            if (args[i].equalsIgnoreCase("-autoThresh")) {
+                autoThresh = true;
+                continue;
+            }
+
+            if (args[i].equalsIgnoreCase("-q")) {
+                silent = true;
+                continue;
+            }
+
+            if (args[i].equalsIgnoreCase("-pages")) {
+                pagesToProcess = new HashSet<Integer>();
+                i++;
+                if (i >= args.length) {
+                    usage();
+                }
+                try {
+                    while (!args[i].equalsIgnoreCase("-pagesEnd")) {
+                        int page = Integer.parseInt(args[i]);
+                        pagesToProcess.add(page);
+                        i++;
+                        if (i >= args.length) {
+                            usage();
+                        }
+                    }
+                } catch (NumberFormatException ex) {
+                    System.err.println("list of page numbers can contain only numbers");
+                    usage();
+                }
+                continue;
+            }
+        }
+
+        if ((jbig2enc == null) || (pdfFile == null)) {
+            usage();
+        }
+
+        if (outputPdf == null) {
+            outputPdf = pdfFile;
+        }
+
+        File originalPdf = new File(pdfFile);
+
+//        System.out.println("Processing " + pdfFile);
+        long sizeOfInputPdf = new File(pdfFile).length();
+        double startTime = System.currentTimeMillis();
+
+        PdfImageProcessor pdfProcessing = new PdfImageProcessor();
+
+        pdfProcessing.extractImagesUsingPdfParser(pdfFile, password, pagesToProcess, silent, binarize);
+//        pdfProcessing.extractImagesUsingPdfObjectAccess(pdfFile, password, pagesToProcess, silent, binarize);
+        List<String> jbig2encInputImages = pdfProcessing.getNamesOfImages();
+        if (jbig2encInputImages.isEmpty()) {
+            if (!silent) {
+                System.out.println("No images in " + pdfFile + " to recompress");
+            }
+//            System.exit(0);
+        }
+        Tools.runJbig2enc(jbig2enc, jbig2encInputImages, defaultThresh, autoThresh, bwThresh, basename, silent);
+
+        List<PdfImageInformation> pdfImagesInfo = pdfProcessing.getOriginalImageInformations();
+        Jbig2ForPdf pdfImages = new Jbig2ForPdf(".", basename);
+        pdfImages.setJbig2ImagesInfo(pdfImagesInfo);
+
+        OutputStream out = null;
+
+        try {
+            File fileName = new File(outputPdf);
+
+            if (fileName.createNewFile()) {
+                System.out.println("file " + outputPdf + " was created");
+            } else {
+                System.out.println("file " + outputPdf + " already exist => will be rewriten");
+            }
+            out = new FileOutputStream(fileName);
+            pdfProcessing.replaceImageUsingIText(pdfFile, out, pdfImages, silent);
+            long sizeOfOutputPdf = fileName.length();
+            float saved = (((float) (sizeOfInputPdf - sizeOfOutputPdf)) / sizeOfInputPdf) * 100;
+            System.out.println("Size of pdf before recompression = " + sizeOfInputPdf);
+            System.out.println("Size of pdf file after recompression = " + sizeOfOutputPdf);
+            System.out.println("=> Saved " + String.format("%.2f", saved) + " % from original size");
+        } catch (IOException ex) {
+            if (!silent) {
+                System.err.println("writing output to the file caused error");
+                ex.printStackTrace();
+            }
+            System.exit(2);
+        } finally {
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException ex2) {
+                    if (!silent) {
+                        ex2.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        int time = (int) (System.currentTimeMillis() - startTime) / 1000;
+        int hour = time / 3600;
+        int min = (time % 3600) / 60;
+        int sec = (time % 3600) % 60;
+        System.out.print("\n" + pdfFile + " succesfully recompressed in ");
+        System.out.println(String.format("%02d:%02d:%02d", hour, min, sec));
+        System.out.println("Totaly was recompressed " + pdfImages.getMapOfJbig2Images().size() + " images");
+
+    }
+
+
+
+
+
+    /**
+     * write usage of main method
+     */
+    private static void usage() {
+        System.err.println("Usage: -pathToEnc <Path to jbig2enc> -input <pdf file> [OPTIONAL]\n");
+        System.err.println("Mandatory options:\n"
+                + "-pathToEnc <Path to jbig2enc>: path to trigger of jbig2enc (usually file named jbig2)\n"
+                + "-intput <pdf file>: pdf file that should be recompressed\n");
+
+        System.err.println("OPTIONAL parameters:\n"
+                + "-output <outputPdf>: name of output pdf file (if not given used input pdf file\n"
+                + "-passwd <password>: password used for decrypting file\n"
+                + "-thresh <valueOfDefaultThresholding>: value that is set to encoder with switch -t\n"
+                + "-autoThresh: engage automatic thresholding (special comparing between two symbols to make better compression ratio)\n"
+                + "-bw_thresh <value of BW thresholding>: sets value for bw thresholding to encoder (in jbig2enc it is switch -T)\n"
+                + "-pages <list of page numbers> -pagesEnd: list of pages that should be recompressed (taken only pages that exists, other ignored) -- now it is not working\n"
+                + "-binarize: enables to process not bi-tonal images (normally only bi-tonal images are processed and other are skipped)\n"
+                + "-basename <basename> sets the basename for output files of jbig2enc"
+                + "-q: silent mode -- no error output is printed");
+        System.exit(1);
+    }
+}
---
+++ trunk/pdfJbIm/src/main/java/cz/muni/pdfjbim/Tools.java
@@ -0,0 +1,260 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.pdfjbim;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+/**
+ *
+ * @author radim
+ */
+public class Tools {
+
+    /**
+     * run jbig2enc with symbol coding used and output in format suitable for PDF
+     * @param defaultThresh what thresholding value should be used
+     * @param autoThresh if improvement of jbig2enc should be used
+     * @param bwThresh setting black/white thresholding
+     * @param silent turns off error output
+     * @param jbig2enc represents path to jbig2enc
+     * @param imageList list of images to be compressed
+     */
+    public static void runJbig2enc(String jbig2enc, List<String> imageList, double defaultThresh, 
+            Boolean autoThresh, int bwThresh, String basename, Boolean silent) throws PdfRecompressionException {
+        if (jbig2enc == null) {
+            throw new NullPointerException("No path to encoder given!");
+        }
+
+        if (imageList == null) {
+            throw new NullPointerException("imageList");
+        }
+
+
+        if (imageList.isEmpty()) {
+            if (!silent) {
+                System.err.println("there are no images for running jbig2enc at (given list is empty)");
+            }
+            return;
+        }
+
+        String images = "";
+        for (int i = 0; i < imageList.size(); i++) {
+            images = images + " " + imageList.get(i);
+        }
+
+
+        String run = jbig2enc + " -s -p -b " + basename;
+        if (autoThresh) {
+            run += " -autoThresh";
+        }
+
+        run += " -t " + defaultThresh;
+
+        run += " -T " + bwThresh;
+
+        run += images;
+        Runtime runtime = Runtime.getRuntime();
+        Process pr1;
+        try {
+            pr1 = runtime.exec(run);
+            InputStream erStream = pr1.getErrorStream();
+            int exitValue = pr1.waitFor();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(erStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+//                writes only a number of symbols recognised by encoder and number of pages
+//                String[] word = line.split(" ");
+//                for (int i = 0; i < word.length; i++) {
+//                    if (word[i].contains("symbols:")) {
+//                        int differenciator = word[i].indexOf(":");
+//                        String symNum = word[i].substring(differenciator+1);
+//                        System.out.print(";" + symNum);
+//                    }
+//                    if (word[i].contains("pages:")) {
+//                        int differenciator = word[i].indexOf(":");
+//                        String pageNum = word[i].substring(differenciator+1);
+//                        System.out.print(";" + pageNum);
+//                    }
+//                }
+
+
+                System.out.println(line);
+            }
+            if (exitValue != 0) {
+                if (!silent) {
+                    System.err.println(run + " ended with error " + exitValue);
+                }
+                deleteFilesFromList(imageList, silent);
+                System.exit(3);
+            }
+        } catch (IOException ex) {
+            if (!silent) {
+                System.err.println("runJbig2enc caused IOException");
+            }
+            ex.printStackTrace();
+        } catch (InterruptedException ex2) {
+            if (!silent) {
+                ex2.printStackTrace();
+            }
+        } finally {
+            deleteFilesFromList(imageList, silent);
+        }
+    }
+
+    /**
+     * @param filesToDelete list of fileNames to be deleted
+     */
+    public static void deleteFilesFromList(List<String> filesToDelete, boolean silent) {
+        for (int i = 0; i < filesToDelete.size(); i++) {
+            File fileToDelete = new File(filesToDelete.get(i));
+            if (!fileToDelete.delete()) {
+                if (!silent) {
+                    System.err.println("problem to delete file: " + fileToDelete.getPath());
+                }
+            }
+        }
+    }
+
+    /**
+     * @param filesToDelete list of fileNames to be deleted
+     */
+    public static void deleteFilesFromList(File[] filesToDelete, boolean silent) {
+        for (int i = 0; i < filesToDelete.length; i++) {
+            File fileToDelete = filesToDelete[i];
+            if (!fileToDelete.delete()) {
+                if (!silent) {
+                    System.err.println("problem to delete file: " + fileToDelete.getPath());
+                }
+            }
+        }
+    }
+
+
+    /**
+     * copy file
+     * @param fromFile what file should be copied
+     * @param toFile where the file should be copied
+     * @throws IOException
+     */
+    public static void copy(File fromFile, File toFile) throws IOException {
+        
+
+        if (!fromFile.exists()) {
+            throw new IOException("FileCopy: " + "no such source file: "
+                    + fromFile.getName());
+        }
+        if (!fromFile.isFile()) {
+            throw new IOException("FileCopy: " + "can't copy directory: "
+                    + fromFile.getName());
+        }
+        if (!fromFile.canRead()) {
+            throw new IOException("FileCopy: " + "source file is unreadable: "
+                    + fromFile.getName());
+        }
+
+        if (toFile.isDirectory()) {
+            toFile = new File(toFile, fromFile.getName());
+        }
+
+        if (toFile.exists()) {
+            if (!toFile.canWrite()) {
+                throw new IOException("FileCopy: "
+                        + "destination file is unwriteable: " + toFile.getName());
+            }
+            System.out.println("Overwrite existing file " + toFile.getName());
+            System.out.flush();
+        } else {
+            String parent = toFile.getParent();
+            if (parent == null) {
+                parent = System.getProperty("user.dir");
+            }
+            File dir = new File(parent);
+            if (!dir.exists()) {
+                throw new IOException("FileCopy: "
+                        + "destination directory doesn't exist: " + parent);
+            }
+            if (dir.isFile()) {
+                throw new IOException("FileCopy: "
+                        + "destination is not a directory: " + parent);
+            }
+            if (!dir.canWrite()) {
+                throw new IOException("FileCopy: "
+                        + "destination directory is unwriteable: " + parent);
+            }
+        }
+
+        FileInputStream from = null;
+        FileOutputStream to = null;
+        try {
+            from = new FileInputStream(fromFile);
+            to = new FileOutputStream(toFile);
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+
+            while ((bytesRead = from.read(buffer)) != -1) {
+                to.write(buffer, 0, bytesRead); // write
+            }
+        } finally {
+            if (from != null) {
+                try {
+                    from.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (to != null) {
+                try {
+                    to.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * copy all files from source directory to destination directory -- used by JUnit tests
+     * @param sourceDir source directory
+     * @param destinationDir destination directory
+     * @throws IOException
+     */
+    public static void copyDir(File sourceDir, File destinationDir) throws IOException {
+        
+        if (sourceDir == null) {
+            throw new NullPointerException("sourceDir");
+        }
+
+        if (destinationDir == null) {
+            throw new NullPointerException("toDir");
+        }
+
+        if ((!sourceDir.exists()) || (!destinationDir.exists())) {
+            throw new IllegalArgumentException(sourceDir.getPath() + " or "
+                    + destinationDir.getPath() + " doesn't exist");
+        }
+
+        if (!sourceDir.isDirectory())  {
+            throw new IllegalArgumentException(sourceDir.getPath() + " is not a directory");
+        }
+
+        if (!destinationDir.isDirectory()) {
+            throw new IllegalArgumentException(destinationDir.getPath() + " is not a directory");
+        }
+
+        File[] sourceFiles = sourceDir.listFiles();
+        for (int i = 0; i < sourceFiles.length; i++) {
+            if (!sourceFiles[i].isDirectory()) {
+                copy(sourceFiles[i], destinationDir);
+            }
+        }
+    }
+}
